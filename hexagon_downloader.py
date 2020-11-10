59fe31226e395074c2829dd55b7103747a114efb
Jake Adams <jdadams@utah.gov>
2020-11-10 16:50:04 -0700

    feat: get tiles from min/max x/y values

--- hexagon_downloader.py
+++ hexagon_downloader.py
@@ -7,6 +7,7 @@ To use, modify the csv_path, out_dir, and unzip_dir variables and then call the 
 """
 
 import csv
+import datetime
 import getpass
 import os
 import requests
@@ -22,15 +23,16 @@ def progbar(progress, total, postfix=''):
     '''
     Simple output for reporting progress given current progress value and progress value at completion
     '''
-    done = int(50 * progress / total)
+    length = 10
+    done = int(length * progress / total)
     percent = round(100 * progress / total, 2)
-    sys.stdout.write(f'\r[{"#" * done}{"_" * (50 - done)}] {percent}% {postfix}')
+    sys.stdout.write(f'\r[{"#" * done}{"_" * (length - done)}] {percent}% {postfix}')
     sys.stdout.flush()
 
 
 #: modified from Sumit Ghosh,
 #: https://sumit-ghosh.com/articles/python-download-progress-bar/
-def download(session, url, filename):
+def download(url, filename, session=requests):
     '''
     Downloads url to filename using requests, reports via progress bar
     '''
@@ -38,13 +40,13 @@ def download(session, url, filename):
     if os.path.exists(filename):
         out_dir = os.path.dirname(filename)
         out_name = os.path.basename(filename)
-        name = out_name.split['.'][0]
-        extension = out_name.split['.'][0]
+        name = out_name.split('.')[0]
+        extension = out_name.split('.')[0]
 
         if name[-3:-1]:
             counter = int(name[-1])
             counter += 1
-            out_name = name[:-1] + counter
+            out_name = name[:-1] + str(counter)
 
         else:
             out_name = name + '_q1'
@@ -95,49 +97,66 @@ def extract_files(source_dir, unzip_dir):
 
 
 if __name__ == '__main__':
-    csv_path = r'c:/temp/discover_downloads/hex_links.csv'
+
+    quad_word = ''
 
     out_dir = Path(r'c:/temp/discover_downloads/downloads')
     unzip_dir = Path(r'c:/temp/discover_downloads/unzipped')
 
-    print('Reading CSV...')
-    all_links = []
-    with open(csv_path) as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            all_links.append(row)
+    min_x = 6202
+    max_x = 6231
+    min_y = 12365
+    max_y = 12413
+
+    extension = 'zip'
 
     out_dir.mkdir(parents=True, exist_ok=True)
     unzip_dir.mkdir(parents=True, exist_ok=True)
 
-    #: We need to use a session here to preserve the login info for secured links
-    print('Logging in...')
-    with requests.Session() as session:
-        session.post(
-            'https://discover.agrc.utah.gov/login', data={
-                'user': input('Username: '),
-                'password': getpass.getpass()
-            }
-        )
+    base_url = f'https://discover.agrc.utah.gov/login/path/{quad_word}/footprint/30cm_hexagon_utah/hx/15'
 
-        print(f'Downloading {len(all_links)} file{"s" if len(all_links) > 1 else ""} to {out_dir}...')
-        tile_counter = 0
+    all_links = [f'{base_url}/{x}/{y}' for x in range(min_x, max_x + 1) for y in range(min_y, max_y + 1)]
 
-        for row in all_links:
-            pieces = row['url'].split('/')
-            x = pieces[-2]
-            y = pieces[-1]
-            z = pieces[-3]
-            layer = pieces[-5]
+    print(f'Downloading {len(all_links)} file{"s" if len(all_links) > 1 else ""} to {out_dir}...')
+    tile_counter = 0
 
-            filename = f'{layer}_x{x}_y{y}_z{z}.{row["extension"]}'
-            hex_out = out_dir / filename
-            postfix = f'\t{filename}, {tile_counter+1} of {len(all_links)}'
+    #: Rough starting assumption of 15 seconds; will differ based on speeds
+    time_deltas = [datetime.timedelta(seconds=15)]
 
-            progbar(tile_counter, len(all_links), postfix)
-            download(session, row['url'], hex_out)
-            tile_counter += 1
-        progbar(len(all_links), len(all_links), postfix)
+    for link in all_links:
+
+        start = datetime.datetime.now()
+        average_time = sum(time_deltas, datetime.timedelta(0)) / len(time_deltas)
+        remaining_items = len(all_links) - tile_counter + 1
+        remaining_time = str(average_time * remaining_items).split('.', 2)[0]
+
+        pieces = link.split('/')
+        x = pieces[-2]
+        y = pieces[-1]
+        z = pieces[-3]
+        layer = pieces[-5]
+
+        filename = f'{layer}_x{x}_y{y}_z{z}.{extension}'
+        hex_out = out_dir / filename
+        postfix = f'\t{filename}, {tile_counter+1} of {len(all_links)}, {remaining_time} rem. (rough)'
+
+        progbar(tile_counter, len(all_links), postfix)
+        download(link, hex_out)
+        time_deltas.append(datetime.datetime.now() - start)
+
+        tile_counter += 1
+
+    progbar(len(all_links), len(all_links), postfix)
 
     print(f'\nExtracting to {unzip_dir}...')
     extract_files(out_dir, unzip_dir)
+
+#: Provo canyon: 6224 12365
+#: Hobble creek: 6231 12386
+#: South:        6202 12413
+#: x: left to right
+#: y: top to bottom
+#: min x:   6202
+#: max x:   6231
+#: min y:   12365
+#: max y:   12413
