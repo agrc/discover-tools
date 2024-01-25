eb18e4346b0990e8a58da21ca244ace0e931356d
Jake Adams <jdadams@utah.gov>
2024-01-25 10:15:28 -0700

    chore: ruff formatting, min/max x/y comments

--- hexagon_downloader.py
+++ hexagon_downloader.py
@@ -18,10 +18,10 @@ from pathlib import Path
 
 #: Progress bar for download
 #: modified from https://sumit-ghosh.com/articles/python-download-progress-bar/
-def progbar(progress, total, postfix=''):
-    '''
+def progbar(progress, total, postfix=""):
+    """
     Simple output for reporting progress given current progress value and progress value at completion
-    '''
+    """
     length = 10
     done = int(length * progress / total)
     percent = round(100 * progress / total, 2)
@@ -32,15 +32,15 @@ def progbar(progress, total, postfix=''):
 #: modified from Sumit Ghosh,
 #: https://sumit-ghosh.com/articles/python-download-progress-bar/
 def download(url, filename, session=requests):
-    '''
+    """
     Downloads url to filename using requests, reports via progress bar
-    '''
+    """
 
     if os.path.exists(filename):
         out_dir = os.path.dirname(filename)
         out_name = os.path.basename(filename)
-        name = out_name.split('.')[0]
-        extension = out_name.split('.')[0]
+        name = out_name.split(".")[0]
+        extension = out_name.split(".")[0]
 
         if name[-3:-1]:
             counter = int(name[-1])
@@ -48,28 +48,30 @@ def download(url, filename, session=requests):
             out_name = name[:-1] + str(counter)
 
         else:
-            out_name = name + '_q1'
+            out_name = name + "_q1"
 
-    with open(filename, 'wb') as f:
+    with open(filename, "wb") as f:
         response = session.get(url, stream=True)
-        total = response.headers.get('content-length')
+        total = response.headers.get("content-length")
 
         if total is None:
             f.write(response.content)
         else:
             downloaded = 0
             total = int(total)
-            for data in response.iter_content(chunk_size=max(int(total / 1000), 1024 * 1024)):
+            for data in response.iter_content(
+                chunk_size=max(int(total / 1000), 1024 * 1024)
+            ):
                 downloaded += len(data)
                 f.write(data)
                 progbar(downloaded, total)
-                sys.stdout.write('\n')
+                sys.stdout.write("\n")
 
 
 def get_file_list(directory, extensions):
-    '''
+    """
     Returns a list of full filepath for all files in directory with specified extensions
-    '''
+    """
 
     file_list = []
     for dir_name, subdir_list, files in os.walk(directory):
@@ -81,42 +83,51 @@ def get_file_list(directory, extensions):
 
 
 def extract_files(source_dir, unzip_dir):
-    '''
+    """
     Extracts any .zip files from source_dir into unzip_dir. The contents of each .zip are placed in
     unzip_dir; no subfolders are created.
-    '''
+    """
     z_prog = 0
-    zip_list = get_file_list(source_dir, '.zip')
+    zip_list = get_file_list(source_dir, ".zip")
 
     for z in zip_list:
         z_prog += 1
-        print('Extracting {}, {} of {}'.format(z, z_prog, str(len(zip_list))))
+        print("Extracting {}, {} of {}".format(z, z_prog, str(len(zip_list))))
         with zipfile.ZipFile(z) as zip_ref:
             zip_ref.extractall(unzip_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    quad_word = "your_quad_word_here"
 
-    quad_word = ''
+    out_dir = Path(r"c:/temp/discover_downloads/downloads")
+    unzip_dir = Path(r"c:/temp/discover_downloads/unzipped")
 
-    out_dir = Path(r'c:/temp/discover_downloads/downloads')
-    unzip_dir = Path(r'c:/temp/discover_downloads/unzipped')
-
+    #: These come from the download links on the web viewer page; the last three items of the link are /z/x/y.
+    #: For example, https://discover.agrc.utah.gov/login/path/{quad-word}/footprint/15cm_hexagon_utah/hx/15/6225/12384
+    #: gives an x of 6225 and a y of 12384.
+    #: Pan around to find the min/max x/y values for your area of interest.
     min_x = 6199
     max_x = 6200
     min_y = 12311
     max_y = 12313
 
-    extension = 'zip'
+    extension = "zip"
 
     out_dir.mkdir(parents=True, exist_ok=True)
     unzip_dir.mkdir(parents=True, exist_ok=True)
 
-    base_url = f'https://discover.agrc.utah.gov/login/path/{quad_word}/footprint/15cm_hexagon_utah/hx/15'
+    base_url = f"https://discover.agrc.utah.gov/login/path/{quad_word}/footprint/15cm_hexagon_utah/hx/15"
 
-    all_links = [f'{base_url}/{x}/{y}' for x in range(min_x, max_x + 1) for y in range(min_y, max_y + 1)]
+    all_links = [
+        f"{base_url}/{x}/{y}"
+        for x in range(min_x, max_x + 1)
+        for y in range(min_y, max_y + 1)
+    ]
 
-    print(f'Downloading {len(all_links)} file{"s" if len(all_links) > 1 else ""} to {out_dir}...')
+    print(
+        f'Downloading {len(all_links)} file{"s" if len(all_links) > 1 else ""} to {out_dir}...'
+    )
     tile_counter = 0
 
     #: Conservative assumption of 30 seconds; will differ based on speeds
@@ -126,22 +137,21 @@ if __name__ == '__main__':
     failed_links = []
 
     for link in all_links:
-
         try:
             start = datetime.datetime.now()
             average_time = sum(time_deltas, datetime.timedelta(0)) / len(time_deltas)
             remaining_items = len(all_links) - tile_counter + 1
-            remaining_time = str(average_time * remaining_items).split('.', 2)[0]
+            remaining_time = str(average_time * remaining_items).split(".", 2)[0]
 
-            pieces = link.split('/')
+            pieces = link.split("/")
             x = pieces[-2]
             y = pieces[-1]
             z = pieces[-3]
             layer = pieces[-5]
 
-            filename = f'{layer}_x{x}_y{y}_z{z}.{extension}'
+            filename = f"{layer}_x{x}_y{y}_z{z}.{extension}"
             hex_out = out_dir / filename
-            postfix = f'\t{filename}, {tile_counter+1} of {len(all_links)}, {remaining_time} rem. (rough)'
+            postfix = f"\t{filename}, {tile_counter+1} of {len(all_links)}, {remaining_time} rem. (rough)"
 
             progbar(tile_counter, len(all_links), postfix)
             download(link, hex_out)
@@ -153,10 +163,10 @@ if __name__ == '__main__':
             failed_links.append(link)
 
     if failed_links:
-        print('\n\nFailed downloads:')
+        print("\n\nFailed downloads:")
         for link in failed_links:
             print(link)
-        print('\n\n')
+        print("\n\n")
 
-    print(f'\nExtracting to {unzip_dir}...')
+    print(f"\nExtracting to {unzip_dir}...")
     extract_files(out_dir, unzip_dir)
